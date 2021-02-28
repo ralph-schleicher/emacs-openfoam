ee300b1065152594f52f12ae75664b8473f6a938
Ralph Schleicher <rs@ralph-schleicher.de>
2021-02-28 14:04:19 +0000

    Genate file.

---
+++ openfoam-autoloads.el
@@ -0,0 +1,38 @@
+;;; openfoam-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "openfoam" "openfoam.el" (24635 39267 925901
+;;;;;;  494000))
+;;; Generated autoloads from openfoam.el
+
+(autoload 'openfoam-apply-data-file-template "openfoam" "\
+Apply the OpenFOAM data file template to the current buffer.
+See ‘openfoam-data-file-template’ for more information." t nil)
+
+(autoload 'openfoam-insert-data-file-header "openfoam" "\
+Insert an OpenFOAM data file header into the current buffer." t nil)
+
+(autoload 'openfoam-create-case-directory "openfoam" "\
+Create an OpenFOAM case directory.
+
+\(fn DIRECTORY)" t nil)
+
+(autoload 'openfoam-mode "openfoam" "\
+Major mode for OpenFOAM data files.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "openfoam" '("openfoam-")))
+
+;;;***
+
+(provide 'openfoam-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; openfoam-autoloads.el ends here
