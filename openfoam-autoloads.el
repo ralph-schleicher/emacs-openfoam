ce7debf9b2ea34e715b0bdb8b82918f0b8af2361
Ralph Schleicher <rs@ralph-schleicher.de>
2021-03-13 18:41:08 +0000

    Regenerate file.

--- openfoam-autoloads.el
+++ openfoam-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 
 
-;;;### (autoloads nil "openfoam" "openfoam.el" (24638 38004 350627
-;;;;;;  155000))
+;;;### (autoloads nil "openfoam" "openfoam.el" (24653 1594 615433
+;;;;;;  888000))
 ;;; Generated autoloads from openfoam.el
 
 (autoload 'openfoam-apply-data-file-template "openfoam" "\
@@ -12,7 +12,21 @@ Apply the OpenFOAM data file template to the current buffer.
 See ‘openfoam-data-file-template’ for more information." t nil)
 
 (autoload 'openfoam-insert-data-file-header "openfoam" "\
-Insert an OpenFOAM data file header into the current buffer." t nil)
+Insert an OpenFOAM data file header into the current buffer.
+
+With prefix argument, insert the data file header at the current line.
+Otherwise, run ‘openfoam-insert-data-file-header-position-hook’ to find
+a suitable buffer position.  If no hook function is configured, search
+for the ‘Code:’ special comment and insert the data file header after
+it.  If ‘Code:’ is not found, insert the data file header before the
+first dictionary entry.
+
+While looking for a suitable buffer position, the special
+variable ‘limit’ is bound to the buffer position specified by
+‘openfoam-insert-data-file-header-line-limit’.  Whether or not
+a hook function obeys this limit is undefined.
+
+\(fn &optional HERE)" t nil)
 
 (autoload 'openfoam-create-case-directory "openfoam" "\
 Create an OpenFOAM case directory.
