a26959739f28a993eb3f6de0f68267c0ea7ee92f
Ralph Schleicher <rs@ralph-schleicher.de>
2021-04-14 18:45:25 +0000

    Version 0.9.1.

--- openfoam.el
+++ openfoam.el
@@ -4,7 +4,7 @@
 
 ;; Author: Ralph Schleicher <rs@ralph-schleicher.de>
 ;; Keywords: languages
-;; Version: 0.9
+;; Version: 0.9.1
 ;; Package-Requires: ((emacs "25.1"))
 ;; URL: https://github.com/ralph-schleicher/emacs-openfoam
 
