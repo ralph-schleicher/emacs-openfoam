6447c666d7446865860f1490856373d1de4a11fe
Ralph Schleicher <rs@ralph-schleicher.de>
2021-05-02 17:39:40 +0000

    Regenerate file.

--- openfoam-autoloads.el
+++ openfoam-autoloads.el
@@ -3,14 +3,52 @@
 ;;; Code:
 
 
-;;;### (autoloads nil "openfoam" "openfoam.el" (24697 10732 797948
-;;;;;;  256000))
+;;;### (autoloads nil "openfoam" "openfoam.el" (24718 58250 233740
+;;;;;;  790000))
 ;;; Generated autoloads from openfoam.el
 
 (autoload 'openfoam-c++-mode "openfoam" "\
 Major mode for editing OpenFOAM C++ code.
 
 \(fn)" t nil)
+
+(autoload 'openfoam-minor-mode "openfoam" "\
+OpenFOAM minor mode.
+If enabled, display the OpenFOAM menu in the menu bar.
+
+If called interactively, enable Openfoam minor mode if ARG is
+positive, and disable it if ARG is zero or negative.  If called
+from Lisp, also enable the mode if ARG is omitted or nil, and
+toggle it if ARG is `toggle'; disable the mode otherwise.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'openfoam-turn-on-minor-mode "openfoam" "\
+Turn on OpenFOAM minor mode if applicable for the current buffer." nil nil)
+
+(put 'openfoam-global-minor-mode 'globalized-minor-mode t)
+
+(defvar openfoam-global-minor-mode nil "\
+Non-nil if Openfoam-Global minor mode is enabled.
+See the `openfoam-global-minor-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `openfoam-global-minor-mode'.")
+
+(custom-autoload 'openfoam-global-minor-mode "openfoam" nil)
+
+(autoload 'openfoam-global-minor-mode "openfoam" "\
+Toggle Openfoam minor mode in all buffers.
+With prefix ARG, enable Openfoam-Global minor mode if ARG is positive;
+otherwise, disable it.  If called from Lisp, enable the mode if
+ARG is omitted or nil.
+
+Openfoam minor mode is enabled in all buffers where
+`openfoam-turn-on-minor-mode' would do it.
+See `openfoam-minor-mode' for more information on Openfoam minor mode.
+
+\(fn &optional ARG)" t nil)
 
 (autoload 'openfoam-mode "openfoam" "\
 Major mode for OpenFOAM data files.
